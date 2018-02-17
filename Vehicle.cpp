c775a3cf4ea9e57e112f26b8b287fe34b204586f
armando212 <armando212@users.noreply.github.com>
2018-02-17 17:29:47 -0400

    Vehicle.cpp

---
+++ Vehicle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Vehicle.h" //(user defined file)
+
+Vehicle: Vehicle()
+{
+	chassisNum=0;
+	maxspeed=10.0;
+}
+
+void vehicle::set chassisNum (string C)
+{
+	chassisNum = C;
+}
+
+string vehicle::ge chassisNum()
+{
+	//code
+}
+//other methods	
+	
